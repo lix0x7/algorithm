094cfa06b1e5162b2816e9e23cb595ef06137afe
luxxli <luxxli@tencent.com>
2022-04-12 16:37:11 +0800

    add 200

---
+++ 200-number-of-islands.js
@@ -0,0 +1,68 @@
+/**
+ * 200. 岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ *
+ *
+ * 提示：
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ */
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid){
+    const dirty = new Array(grid.length).fill(0).map(x => new Array(grid[0].length).fill(false));
+    let ans = 0;
+    const impl = function (grid, i, j){
+        if (i < 0 || j < 0 || i >= grid[0].length || j >= grid.length || dirty[j][i] || grid[j][i] === '0'){
+            return 0;
+        }
+
+        dirty[j][i] = true;
+        impl(grid, i + 1, j);
+        impl(grid, i -1 , j);
+        impl(grid, i, j + 1);
+        impl(grid, i, j - 1);
+        return 1;
+    }
+
+    for (let j = 0; j < grid.length; ++j){
+        for (let i = 0; i < grid[j].length; ++i){
+            ans += impl(grid, i, j)
+        }
+    }
+
+    return ans;
+};
+
+console.log(numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]));
