7701d9a96814497f834c9908f959e73fd3b7c18b
luxxli <luxxli@tencent.com>
2022-04-13 23:36:35 +0800

    add 3

---
+++ 3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,60 @@
+/**
+ * tag: 双指针
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ *
+ * 提示：
+ *
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const m = new Set();
+    let i = 0, j = 0;
+    let rtn = 0;
+
+    while (j < s.length){
+        const cur = s[j];
+        if (!m.has(cur)){
+            // 移动右指针
+            m.add(cur);
+            rtn = rtn > (j - i + 1) ? rtn : (j - i + 1);
+            j++;
+        }else {
+            // 移动左指针
+            m.delete(s[i]);
+            i++;
+        }
+    }
+    return rtn;
+};
+
+console.log(lengthOfLongestSubstring('123456789'), 9);
+console.log(lengthOfLongestSubstring('123412345'), 5);
+console.log(lengthOfLongestSubstring('12121212312'), 3);
+console.log(lengthOfLongestSubstring('dvdf'), 3);
