5b74f6eb23c3f3fca5f42a4ccb48b3c16714c9a1
luxxli <luxxli@tencent.com>
2022-05-07 00:19:15 +0800

    add 79

---
+++ 79-binary-tree-inorder-traversal.js
@@ -0,0 +1,56 @@
+/**
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const rtn = [];
+    const impl = function (cur){
+        if (!cur){
+            return;
+        }
+        impl(cur.left);
+        rtn.push(cur.val);
+        impl(cur.right);
+    }
+    impl(root);
+    return rtn;
+};
+
+/**
+ * tag 树
+ */
