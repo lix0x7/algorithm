d67d5e32eab3403e997cfbbd0b4b33a73acb1770
luxxli <luxxli@tencent.com>
2022-06-06 23:58:52 +0800

    optimize 240

--- 240-search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii.js
@@ -36,27 +36,16 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-
-  const search = function (nums, target){
-    let i = 0, j = nums.length - 1;
-    while (i <= j){
-      let mid = i + Math.floor((j - i) / 2);
-      if (nums[mid] === target){
-        return true;
-      }else if (nums[mid] > target){
-        j = mid - 1;
-      }else {
-        i = mid + 1;
-      }
-    }
-    return false;
-  }
-
-  matrix = matrix.filter(x => x[0] <= target).map(x => x.filter(y => y <= target));
-  console.log(matrix);
-  for (const nums of matrix){
-    if (search(nums, target)){
+  let x = 0, y = matrix[0].length - 1;
+  while (x < matrix.length && y >= 0) {
+    const cur = matrix[x][y];
+    if (cur === target) {
       return true;
+    }
+    if (cur > target) {
+      y--;
+    } else {
+      x++;
     }
   }
   return false;
@@ -65,5 +54,8 @@ var searchMatrix = function(matrix, target) {
 console.log(searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5), true);
 
 /**
- * tag 二分查找
+ * tag 错题 二分查找
+ *
+ * 不太好归类，Z字形搜索，很trick，个人感觉这题能想到二分其实就够了
+ *
  */
