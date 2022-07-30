2bdcc8e946d229e5c0d1801c1c248b381f7b441b
luxxli <luxxli@tencent.com>
2022-07-30 23:31:25 +0800

    add 572

---
+++ 572-subtree-of-another-tree.js
@@ -0,0 +1,58 @@
+/**
+ * 572. Subtree of Another Tree
+ * Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+ *
+ * A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+ * Output: true
+ * Example 2:
+ *
+ *
+ * Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the root tree is in the range [1, 2000].
+ * The number of nodes in the subRoot tree is in the range [1, 1000].
+ * -104 <= root.val <= 104
+ * -104 <= subRoot.val <= 104
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+const same = function (t1, t2){
+  if (!t1 && !t2){
+    return true;
+  }
+  if (t1 && t2 && t1.val === t2.val){
+    return same(t1.left, t2.left) && same(t1.right, t2.right);
+  }
+  return false;
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+  if (!root && subRoot){
+    return false;
+  }
+   return same(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
