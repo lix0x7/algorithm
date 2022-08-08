e59b301dd0a6237abd2a56b069d18734229ac73e
luxxli <luxxli@tencent.com>
2022-08-08 14:23:45 +0800

    add 143

---
+++ 143-reorder-list.js
@@ -0,0 +1,62 @@
+/**
+ * 143. Reorder List
+ * You are given the head of a singly linked-list. The list can be represented as:
+ *
+ * L0 → L1 → … → Ln - 1 → Ln
+ * Reorder the list to be on the following form:
+ *
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+ *
+ *
+ *
+ * Example 1:
+ * https://assets.leetcode.com/uploads/2021/03/04/reorder1linked-list.jpg
+ * Input: head = [1,2,3,4]
+ * Output: [1,4,2,3]
+ *
+ * Example 2:
+ * https://assets.leetcode.com/uploads/2021/03/09/reorder2-linked-list.jpg
+ * Input: head = [1,2,3,4,5]
+ * Output: [1,5,2,4,3]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [1, 5 * 104].
+ * 1 <= Node.val <= 1000
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+  let cur = head, s = [];
+  while (cur){
+    s.push(cur);
+    cur = cur.next;
+  }
+
+  let i = 0, j = s.length - 1;
+  while (i < j){
+    let l = s[i], r = s[j];
+    const nn = l.next;
+    l.next = r;
+    r.next = nn;
+    i++;
+    j--;
+  }
+  s[i].next = null;
+};
+
+/**
+ * tag 优雅解法
+ */
