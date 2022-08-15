ababa09845ec25f010ac02c0d45455f266d1d264
luxxli <luxxli@tencent.com>
2022-08-15 20:33:04 +0800

    add 190

---
+++ 190-reverse-bits.js
@@ -0,0 +1,58 @@
+/**
+ * 190. Reverse Bits
+ * Reverse bits of a given 32 bits unsigned integer.
+ *
+ * Note:
+ *
+ * Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+ * In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 00000010100101000001111010011100
+ * Output:    964176192 (00111001011110000010100101000000)
+ * Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+ * Example 2:
+ *
+ * Input: n = 11111111111111111111111111111101
+ * Output:   3221225471 (10111111111111111111111111111111)
+ * Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+ *
+ *
+ * Constraints:
+ *
+ * The input must be a binary string of length 32
+ *
+ *
+ * Follow up: If this function is called many times, how would you optimize it?
+ */
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+  n = n >>> 0;
+  let offset = 0;
+  while (true){
+    let highMask = (1 << (31 - offset)) >>> 0;
+    let lowMask  = 1 << offset;
+    if (highMask <= lowMask){
+      return n >>> 0;
+    }
+
+    let highBackup = n & highMask;
+    // console.log(offset, (n & highMask).toString(2), (n & lowMask).toString(2));
+    n = (n & ~highMask) | ((n & lowMask) << (31 - 2 * offset));
+    n = (n & ~lowMask)  | (highBackup >>> (31 - 2 * offset));
+
+    offset += 1;
+  }
+};
+
+console.log(reverseBits(0b00000010100101000001111010011100).toString(2), '\n111001011110000010100101000000');
+
+/**
+ * tag 位运算 双指针
+ */
