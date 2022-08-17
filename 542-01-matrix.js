fc901665df4f03bb03447189d123f9aeff27df21
luxxli <luxxli@tencent.com>
2022-08-17 13:00:59 +0800

    optimize 542

--- 542-01-matrix.js
+++ 542-01-matrix.js
@@ -34,29 +34,29 @@
 var updateMatrix = function(mat) {
   if (mat.length === 0) return [];
   const dp = new Array(mat.length).fill(0).map(x => new Array(mat[0].length).fill(Number.MAX_SAFE_INTEGER));
-  dp[-1] = [];
-  dp[mat.length] = [];
+
+  // 遍历左侧和上侧
   for (let i = 0; i < mat.length; ++i){
     for (let j = 0; j < mat[0].length; ++j){
       if (mat[i][j] === 0){
         dp[i][j] = 0;
       }else {
-        dp[i][j] = Math.min(dp[i][j], 1 + (dp[i - 1][j] ?? Number.MAX_SAFE_INTEGER), 1 + (dp[i][j - 1] ?? Number.MAX_SAFE_INTEGER));
+        dp[i][j] = Math.min(dp[i][j], 1 + (dp[i - 1]?.[j] ?? Number.MAX_SAFE_INTEGER), 1 + (dp[i]?.[j - 1] ?? Number.MAX_SAFE_INTEGER));
       }
     }
   }
 
+  // 遍历右侧和下册
   for (let i = mat.length - 1; i >= 0; --i){
     for (let j = mat[0].length - 1; j >= 0; --j){
       if (mat[i][j] === 0){
         dp[i][j] = 0;
       }else {
-        dp[i][j] = Math.min(dp[i][j], 1 + (dp[i + 1][j] ?? Number.MAX_SAFE_INTEGER), 1 + (dp[i][j + 1] ?? Number.MAX_SAFE_INTEGER));
+        dp[i][j] = Math.min(dp[i][j], 1 + (dp[i + 1]?.[j] ?? Number.MAX_SAFE_INTEGER), 1 + (dp[i]?.[j + 1] ?? Number.MAX_SAFE_INTEGER));
       }
     }
   }
 
-  dp.pop();
   return dp;
 };
 
@@ -64,4 +64,5 @@ var updateMatrix = function(mat) {
 /**
  * tag 错题本 图 动态规划
  * 暴力解必然超时
+ * 很有意思，这是一道十分说明备忘录递归和dp等价的题目，只不过平时我们不会用dp去处理图问题
  */
